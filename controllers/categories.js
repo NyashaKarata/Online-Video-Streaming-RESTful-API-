13524f2789e8f1e46b9cffe15c4d8a805192ac32
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-10-29 21:26:54 +0200

    Update categories.js

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,7 +2,7 @@ const asyncHandler = require('../middleware/async')
 const ErrorResponse = require('../utils/errorResponse')
 const Category = require('../models/Category')
 
-// @desc    Get categori
+// @desc    Get categor
 // @route   GET /api/v1/categories
 // @access  Private/Admin
 exports.getCategories = asyncHandler(async (req, res, next) => {
