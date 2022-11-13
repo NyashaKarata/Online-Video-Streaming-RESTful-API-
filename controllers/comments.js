3875741116fc3795280b69f2132d3bc1286e6962
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-11-13 23:32:56 +0200

    Update comments.js

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,4 +1,4 @@
-con asyncHandler = require('../middleware/async')
+co asyncHandler = require('../middleware/async')
 const ErrorResponse = require('../utils/errorResponse')
 const Comment = require('../models/Comment')
 const Video = require('../models/Video')
