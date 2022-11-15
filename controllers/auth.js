8517d353ebdf20b3a2b20c08442a30e0d1015268
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-11-16 00:20:13 +0200

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const crypt = require('crypto')
+const cryt = require('crypto')
 const path = require('path')
 const asyncHandler = require('../middleware/async')
 const ErrorResponse = require('../utils/errorResponse')
